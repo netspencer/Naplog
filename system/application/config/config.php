d3da6f2961b262d85b20f75d6b3ba4437ba3b4dd
Spencer Schoeben <spencer@netspencer.com>
2010-12-09 16:18:35 -0800

    Edited commit m essage to add ability for it to automatical set base_url. Tweeked css for header

--- system/application/config/config.php
+++ system/application/config/config.php
@@ -11,7 +11,8 @@
 |	http://example.com/
 |
 */
-$config['base_url']	= "http://naplog.com/";
+$config['base_url']	= 'http' . ((isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] == 'on') ? 's' : '')
+                      .'://'.$_SERVER['HTTP_HOST'].str_replace('//','/',dirname($_SERVER['SCRIPT_NAME']).'/');
 
 /*
 |--------------------------------------------------------------------------
