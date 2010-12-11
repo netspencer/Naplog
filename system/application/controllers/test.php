48ff883ba70df876595427176691afdb249c97de
Spencer Schoeben <spencer@netspencer.com>
2010-12-10 20:33:00 -0800

    Testing a little feature to find out if someone is cool. Moslty just to see if Kfir sees this

--- system/application/controllers/test.php
+++ system/application/controllers/test.php
@@ -19,6 +19,17 @@ class Test extends Controller {
 		$this->follow->follow(12);
 	}
 	
+	function cool($person) {
+	  $cool_people[] = "spencer";
+	  $cool_people[] = "kfir";
+	  
+	  if (in_array($person, $cool_people)) {
+	    echo "Yeah, $person is cool!";
+	  } else {
+	    echo "Sadly, $person isn't so cool.";
+	  }
+	}
+	
 }
 
 ?>
