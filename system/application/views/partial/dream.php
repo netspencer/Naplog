2a37a89146132afa71b57858e8c0c2d4bc651e9a
Kfir Dolev <bazilikan@gmail.com>
2011-01-28 21:26:25 -0800

    made comment button actually take you to comments

--- system/application/views/partial/dream.php
+++ system/application/views/partial/dream.php
@@ -12,7 +12,7 @@
 		<h3 class="action"><a href="<?=base_url()?>user/<?=$username?>"><?=$fullname?></a> &mdash; <a href="<?=base_url()?>dream/<?=$dream_id?>"><span class="timestamp" title="<?=$iso_timestamp?>"><?=$full_timestamp?></span></a></h3>
 		<ul class="buttons">
 			<li><a rel="like" <?php if($user_liked): ?>class="liked"<?php endif;?> href="#"><?=$num_likes?> likes</a></li>
-			<li><a href="#add-comment" rel="comment"><?=$num_comments?> comments</a></li>
+			<li><a href="<?=base_url()?>dream/<?=$dream_id?>" rel="comment"><?=$num_comments?> comments</a></li>
 		</ul>
 		<div class="clear"></div>
 	</div>
