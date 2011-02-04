426b9a8e303594cb4c7757d1bed00d236a870e5d
Kfir Dolev <bazilikan@gmail.com>
2011-02-03 22:32:11 -0800

    changed mininavbar and dashboard

--- system/application/views/page/dashboard.php
+++ system/application/views/page/dashboard.php
@@ -1,47 +1,63 @@
 <section id="new_dream">
 	<?=form_open("dashboard")?>
 		<div class="container_12">
-			<div class="grid_12">
-				<div class="banner"><p>Good morning, <?=$nickname?>.</p></div>
+			<div class="grid_9">
 				<div class="errors"><?=validation_errors()?></div>
 			</div>
+			<div class="dasboard_navbar_fancyness grid_9">
+				<p>Hi thar</p>
+			</div>
+			<div class="dashboard_navbar grid_9">
+				<ul class="dashboard_navbar">
+					<li class="dashboard_navbar"><a class="dashboard_navbar" href="">Log Dream</a></li>
+					<li class="dashboard_navbar"><a class="dashboard_navbar" href="">My Dreams</a></li>
+					<li class="dashboard_navbar"><a class="dashboard_navbar" href="">My Analytics</a></li>		
+			</div>
 			<div class="clear"></div>
+			<div class="post_dream grid_9">
 			
-			<div class="grid_7">
-				<div class="widget describe">
-					<h2>Describe your dream last night.</h2>
-						<textarea name="content"></textarea>
-				</div>
-			</div>
-		
-			<div class="grid_5">
+				<div class="clear"></div>
 				
-				<div class="widget rate">
-					<h2>How many hours did you sleep?</h2>
-				
-					<div class="sleep"></div>
-					<input type="hidden" name="sleep" value="8" />
-				
-					<ul>
-						<li rel="1" class="d1">1</li>
-						<li rel="2" class="d2">2</li>
-						<li rel="3" class="d3">3</li>
-						<li rel="4" class="d4">4</li>
-						<li rel="5" class="d5">5</li>
-						<li rel="6" class="d6">6</li>
-						<li rel="7" class="d7">7</li>
-						<li rel="8" class="d8 current">8</li>
-						<li rel="9" class="d9">9</li>
-						<li rel="10" class="d10">10</li>
-						<li rel="11" class="d11">11</li>
-						<li rel="12" class="d12">12</li>
-						<div class="clear"></div>							
-					</ul>
+            	
+		    	
+				<div class="grid_9">
 					
-					<input type="submit" />	
+					<div class="widget rate">
+						<h2>How many hours did you sleep?</h2>
+					
+						<div class="sleep"></div>
+						<input type="hidden" name="sleep" value="8" />
+					
+						<ul>
+							<li rel="1" class="d1">1</li>
+							<li rel="2" class="d2">2</li>
+							<li rel="3" class="d3">3</li>
+							<li rel="4" class="d4">4</li>
+							<li rel="5" class="d5">5</li>
+							<li rel="6" class="d6">6</li>
+							<li rel="7" class="d7">7</li>
+							<li rel="8" class="d8 current">8</li>
+							<li rel="9" class="d9">9</li>
+							<li rel="10" class="d10">10</li>
+							<li rel="11" class="d11">11</li>
+							<li rel="12" class="d12">12</li>
+							<div class="clear"></div>							
+						</ul>
+						
+					</div>
 				</div>
+				
+				<div class="grid_9">
+					<div class="widget describe">
+						<h2>Describe your dream last night.</h2>
+							<textarea name="content"></textarea>
+							
+					</div>
+
+				</div>
+				
+				<div class="clear"></div>
 			</div>
-			<div class="clear"></div>
 		</div>
 	<?=form_close()?>
 </section>
--- system/application/views/page/dreams.php
+++ system/application/views/page/dreams.php
@@ -1,6 +1,9 @@
 <section id="dreams">
 	<div class="container_12">
-		<div class="grid_8">
+		<div class="grid_9">
+			<div class="fancyness">
+				<p>Sort by</p>
+			</div>
 			<ul class="sections">
 				<li <?php if($current_section=="following") echo "class=\"current\""?>><a href="<?=base_url()?>dreams/following">Following</a></li>
 				<li <?php if($current_section=="popular") echo "class=\"current\""?>><a href="<?=base_url()?>dreams/popular">Popular</a></li>
@@ -9,10 +12,15 @@
 			<?php $this->load->view("inc/list_dreams", array("dreams"=>$dreams));?>
 			<a href="#" id="load_more">Load More Dream</a>
 		</div>
-		<div class="grid_4">
-			<div class="ad">	
-				<h3>Advertisement</h3>
-				<a href="http://klassio.com"><img src="http://f.cl.ly/items/3n2W1o321z3Q2M0D2t14/klassio-ad.jpg" alt="ad" /></a>
+		<div class="grid_3">
+			<div class="fancyness">
+				<p>Ads</p>
+			</div>
+			<div class="sidebar">
+				<div class="ad">	
+					<h3>Advertisement</h3>
+					<a href="http://klassio.com"><img src="http://f.cl.ly/items/3n2W1o321z3Q2M0D2t14/klassio-ad.jpg" alt="ad" /></a>
+				</div>
 			</div>
 		</div>
 		<div class="clear"></div>
